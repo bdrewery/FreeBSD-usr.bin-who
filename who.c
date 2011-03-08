47f5906ea3cb7b725cbf044b371410cf0086d0a3
Bryan Drewery <bryan@shatow.net>
2011-03-08 14:03:55 -0600

    Update to FreeBSD 7.4

--- who.c
+++ who.c
@@ -25,7 +25,7 @@
  */
 
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/usr.bin/who/who.c,v 1.22.12.1 2010/02/10 00:26:20 kensmith Exp $");
+__FBSDID("$FreeBSD: src/usr.bin/who/who.c,v 1.22.14.1 2010/12/21 17:10:29 kensmith Exp $");
 
 #include <sys/param.h>
 #include <sys/types.h>
